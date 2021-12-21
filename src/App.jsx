534fb0ac88e82f2291cf5ad247b1f4ec7bf6d5b2
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-21 20:52:42 +0900

    再レンダリングの確認

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
+  );
+}
