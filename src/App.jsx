5f537af42d19b0c2606f513d12c03db4b194d255
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-22 10:43:46 +0900

    ファイルリセット

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,5 @@
-import { useState, useCallback, useMemo } from "react";
-import { ChildArea } from "./ChildArea";
-import "./styles.css";
+import React, { useState } from "react";
 
-export default function App() {
-  console.log("App");
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
-  const onChangeText = (e) => {
-    setText(e.target.value);
-  };
-
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
-  const onClickClose = useCallback(() => {
-    setOpen(false);
-  }, []);
-  return (
-    <div>
-      <input type="text" value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
-    </div>
-  );
-}
+export const App = () => {
+  return <></>;
+};
