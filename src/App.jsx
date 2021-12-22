5bac51fb6780e3c0fbc566042423191b44970524
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-22 11:10:58 +0900

    css modulus

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { InlineStyle } from "./InlineStyle";
+import { CssModules } from "./components/CssModules";
+import { InlineStyle } from "./components/InlineStyle";
 
 export const App = () => {
   return (
     <div>
       <InlineStyle />
+      <CssModules />
     </div>
   );
 };
