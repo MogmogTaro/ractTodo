b3977727fc38c5916fa41cb2b71a1e7deffb0616
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-23 16:58:07 +0900

    Organismsその１

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メールアドレス</dt>
+        <dd>1111@aaaaa.com</dd>
+        <dt>TEL</dt>
+        <dd>090-1234-5678</dd>
+        <dt>会社名</dt>
+        <dd>ああああ株式会社</dd>
+        <dt>WEB</dt>
+        <dd>https....com</dd>
+      </dl>
+    </div>
+  );
+};
