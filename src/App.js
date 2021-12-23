0f9d987b0f951faa7e5f7b15b66bb682058ca447
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-23 23:45:56 +0900

    pageの作成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { BrowserRouter } from "react-router-dom";
-
-const user = {
-  image: "https://source.unsplash.com/1zmdbC-8T6s",
-  name: "うちだ",
-  email: "1234@example.com",
-  phone: "090-1234-5678",
-  company: {
-    name: "テスト株式会社",
-    website: "https://google.com"
-  }
-};
+import { Router } from "./router/Router";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
