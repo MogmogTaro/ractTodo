663afb3693268af3b6c8c2c6c156320336425cc6
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2021-12-24 19:35:49 +0900

    useContextの最適化

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,12 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
   const { children } = props;
+
+  const [userInfo, setUserInfo] = useState(null);
   const contextName = "うちだま";
   return (
-    <UserContext.Provider value={{ contextName }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   );
